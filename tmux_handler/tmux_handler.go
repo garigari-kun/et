18fa175a2aef5ddb91325c74d7044698b5f8a6d9
garigari-kun <keisuke.cs@gmail.com>
2019-07-01 21:48:01 +0900

    Add some styling!

--- tmux_handler/tmux_handler.go
+++ tmux_handler/tmux_handler.go
@@ -1,121 +1,130 @@
 package tmux_handler
 
 import (
-  "log"
-  "os"
-  "os/exec"
-  "strings"
-  "strconv"
-  "fmt"
-  "bufio"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 type Session struct {
-  Id int
-  Name string
-  Attached string 
-} 
+	Id       int
+	Name     string
+	Attached string
+}
 
 type Sessions []Session
 
-func SetTmuxSessions() Sessions  {
-  out, err := exec.Command("sh", "-c", "tmux ls  -F '#{session_name}:#{session_attached}'").Output()
-  if err != nil {
-    log.Print(err)
-  }
-  splited_out := strings.Fields(string(out))
+const (
+	InfoColor    = "\033[1;34m%s\033[0m"
+	NoticeColor  = "\033[1;36m%s\033[0m"
+	WarningColor = "\033[1;33m%s\033[0m"
+	ErrorColor   = "\033[1;31m%s\033[0m"
+	DebugColor   = "\033[0;36m%s\033[0m"
+)
 
-  var session_slice Sessions
-  for index, session := range splited_out {
-    splited_sessions := strings.Split(session, ":")
-    session := Session{Id: index + 1, Name: splited_sessions[0], Attached: splited_sessions[1]}
-    session_slice = append(session_slice, session)
-  }
-  return session_slice
+func SetTmuxSessions() Sessions {
+	out, err := exec.Command("sh", "-c", "tmux ls  -F '#{session_name}:#{session_attached}'").Output()
+	if err != nil {
+		log.Print(err)
+	}
+	splited_out := strings.Fields(string(out))
+
+	var session_slice Sessions
+	for index, session := range splited_out {
+		splited_sessions := strings.Split(session, ":")
+		session := Session{Id: index + 1, Name: splited_sessions[0], Attached: splited_sessions[1]}
+		session_slice = append(session_slice, session)
+	}
+	return session_slice
 }
 
 func IsSessionAttached(sessions Sessions) bool {
-  for _, session := range sessions {
-    if session.Attached == "1" {
-      return true
-    }
-  }
-  return false 
+	for _, session := range sessions {
+		if session.Attached == "1" {
+			return true
+		}
+	}
+	return false
 }
 
 func FindSessionById(sessions Sessions, id string) string {
-  for _, session := range sessions {
-    if strconv.Itoa(session.Id) == id {
-      return session.Name
-    }
-  }
-  log.Print("Can't find session name the id you entered.")
-  return "false"
+	for _, session := range sessions {
+		if strconv.Itoa(session.Id) == id {
+			return session.Name
+		}
+	}
+	log.Print("Can't find session name the id you entered.")
+	return "false"
 }
 
 func AttachSession(session_name string) {
-  var attach_cmd *exec.Cmd
-  attach_cmd = exec.Command("tmux", "attach", "-t", session_name)
-  attach_cmd.Stdin = os.Stdin
-  attach_cmd.Stdout = os.Stdout
-  attach_cmd.Stderr = os.Stderr
-  err := attach_cmd.Run()
-  if err != nil {
-    log.Print(err)
-  }
+	var attach_cmd *exec.Cmd
+	attach_cmd = exec.Command("tmux", "attach", "-t", session_name)
+	attach_cmd.Stdin = os.Stdin
+	attach_cmd.Stdout = os.Stdout
+	attach_cmd.Stderr = os.Stderr
+	err := attach_cmd.Run()
+	if err != nil {
+		log.Print(err)
+	}
 }
 
 func CreateNewSession(new_session string) {
-  var attach_cmd *exec.Cmd
-  attach_cmd = exec.Command("tmux", "new", "-s", new_session, "-d")
-  attach_cmd.Stdin = os.Stdin
-  attach_cmd.Stdout = os.Stdout
-  attach_cmd.Stderr = os.Stderr
-  err := attach_cmd.Run()
-  if err != nil {
-    log.Print(err)
-  }
+	var attach_cmd *exec.Cmd
+	attach_cmd = exec.Command("tmux", "new", "-s", new_session, "-d")
+	attach_cmd.Stdin = os.Stdin
+	attach_cmd.Stdout = os.Stdout
+	attach_cmd.Stderr = os.Stderr
+	err := attach_cmd.Run()
+	if err != nil {
+		log.Print(err)
+	}
 }
 
 func SwitchSession(new_session string) {
-  var attach_cmd *exec.Cmd
-  attach_cmd = exec.Command("tmux switch-client -t " + new_session)
-  attach_cmd = exec.Command("tmux", "switch-client", "-t", new_session)
-  attach_cmd.Stdin = os.Stdin
-  attach_cmd.Stdout = os.Stdout
-  attach_cmd.Stderr = os.Stderr
-  err := attach_cmd.Run()
-  if err != nil {
-    log.Print(err)
-  }
+	var attach_cmd *exec.Cmd
+	attach_cmd = exec.Command("tmux switch-client -t " + new_session)
+	attach_cmd = exec.Command("tmux", "switch-client", "-t", new_session)
+	attach_cmd.Stdin = os.Stdin
+	attach_cmd.Stdout = os.Stdout
+	attach_cmd.Stderr = os.Stderr
+	err := attach_cmd.Run()
+	if err != nil {
+		log.Print(err)
+	}
 }
 
 func ListChoicesToTerminal(sessions Sessions) {
-  fmt.Println("Create new session or Attach another session.")
-  fmt.Println("0: Create New Session")
-  for _, session := range sessions {
-    var list string
-    if session.Attached == "1" {
-      list = strconv.Itoa(session.Id) + ": " + session.Name + " (Attached)"
-    } else {
-      list = strconv.Itoa(session.Id) + ": " + session.Name
-    }
-    fmt.Println(list)
-  }
+	fmt.Printf(NoticeColor, "=====Create new session or Attach another session=====\n")
+	fmt.Printf(WarningColor, "0: Create New Session\n")
+	for _, session := range sessions {
+		var list string
+		if session.Attached == "1" {
+			list = strconv.Itoa(session.Id) + ": " + session.Name + " (Attached)"
+		} else {
+			list = strconv.Itoa(session.Id) + ": " + session.Name
+		}
+		fmt.Println(list)
+	}
+	fmt.Printf(NoticeColor, "======================================================\n")
 }
 
 func PromptUserChoice() string {
-  scanner := bufio.NewScanner(os.Stdin)
-  fmt.Println("Enter what you want: ")
-  scanner.Scan()
-  text := scanner.Text()
-  return text
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf(InfoColor, "Enter what you want: ")
+	scanner.Scan()
+	text := scanner.Text()
+	return text
 }
 
 func PromptUserToNewSessionName() string {
-  scanner := bufio.NewScanner(os.Stdin)
-  fmt.Println("Enter new session name: ")
-  scanner.Scan()
-  new_session_name := scanner.Text()
-  return new_session_name
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Enter new session name: ")
+	scanner.Scan()
+	new_session_name := scanner.Text()
+	return new_session_name
 }
