547e89fce712e31ea3d9210d9fcd71c56b12bd9c
garigari-kun <keisuke.cs@gmail.com>
2019-09-16 11:48:14 +0900

    Add whether session is attached or not handling

--- cmd/window.go
+++ cmd/window.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"log"
+	"os"
 
+	"github.com/garigari-kun/et/tmux_handler"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +14,13 @@ func WindowCmd() *cobra.Command {
 		Short: "Tmux window",
 		Run: func(cmd *cobra.Command, args []string) {
 			log.Print("Window command is called")
+			sessions := tmux_handler.NewTmuxSessions()
+			if sessions.IsSessionAttached() {
+				log.Print("session is attached")
+			} else {
+				log.Print("Session is not attached. Can't create window")
+				os.Exit(1)
+			}
 		},
 	}
 
